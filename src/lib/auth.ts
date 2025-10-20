31650ce1e944ea02c2d09f299abe27b5831e28cf
Aditi Guptee <aditiguptee@Aditis-Laptop.local>
2025-10-19 21:48:28 -0700

    new updates to the site

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -65,13 +65,13 @@ const config = {
     strategy: 'jwt' as const,
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: any; user: any }) {
       if (user) {
         token.role = user.role
       }
       return token
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: any; token: any }) {
       if (token) {
         session.user.id = token.sub!
         session.user.role = token.role
